6dcdcd2faba429421db24eb1fb09ffbc1efc4bef
Gabriel Bernabé <gabi.bernabe17@gmail.com>
2025-07-16 14:49:30 -0300

    Corrección final para el proyecto del sprint 7

--- configuration.py
+++
@@ -1,4 +0,0 @@
-URL_SERVICE = "https://cnt-24071104-3f47-46ac-a499-30b7792ce4bc.containerhub.tripleten-services.com"
-CREATE_USER_PATH = "/api/v1/users"
-KITS_PATH = "/api/v1/kits"
-DOCS_URL = "https://cnt-24071104-3f47-46ac-a499-30b7792ce4bc.containerhub.tripleten-services.com/docs/"
